0b10baad0370a98135d0d5c7f89b4448935bf6a3
Your Name <happyeyal@gmail.com>
2020-11-15 20:40:53 +0200

    fixing power and exponent functions

--- power.c
+++ power.c
@@ -1,19 +1,29 @@
 #include "myMath.h"
 #define  E 2.7182818284590452354 
 
-double Exponent(int x){
-    double ans = 1;
-    for (int i = 0; i < x; i++)
-    {
-        ans *= E;
-    }
-    return ans;
-}
 double Power(double x , int y){
-    double ans = 1;
-    for (int i = 0; i < y; i++)
-    {
-        ans *= x;
+    if(y<0){
+        return 1/Power(x,-y);
     }
-    return ans;
+    else{
+        double ans = 1;
+        for (int i = 0; i < y; i++)
+        {
+            ans *= x;
+        }
+        return ans;
+    }
+}
+double Exponent(int x){
+    if(x<0){
+        return 1/Exponent(-x);
+    }
+    else{
+        double ans = 1;
+        for (int i = 0; i < x; i++)
+        {
+            ans *= E;
+        }
+        return ans;
+    }
 }
